b196ceb50c8e0898551683c1abc8cf0982db3c3a
Dhrumal Patel <87417817+Dhrumal-Patel@users.noreply.github.com>
2025-06-30 23:26:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Config:
         self.COMMUNICATION_ROUNDS = 25
         self.CLIENT_EPOCHS = 10
         self.LEARNING_RATE = 0.01
-        self.HIDDEN_SIZE_BINARY = 128  
-        self.HIDDEN_SIZE_MULTICLASS = 256 
+        self.HIDDEN_SIZE_BINARY = 256  
+        self.HIDDEN_SIZE_MULTICLASS = 512 
         self.NUM_LAYERS = 2
         self.DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.FALL_SCENARIOS = [5, 4, 10, 0]
